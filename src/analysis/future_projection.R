f502ff381b8a3e315cd07425912f1149755f65c1
James Rising <jarising@gmail.com>
2021-03-28 17:34:51 -0400

    code for future projection

---
+++ src/analysis/future_projection.R
@@ -0,0 +1,49 @@
+## setwd("~/research/scciams/scc_structural")
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+## Prepare data
+source("src/data_cleaining_scripts/cleaning_standardizedollaryears.R")
+
+perref <- data.frame(table(df$Reference))
+names(perref) <- c("Reference", "rows")
+
+df2 <- dat %>% left_join(perref)
+df2$`SCC Year` <- as.numeric(df2$`SCC Year`)
+
+df2$log.scc.2020usd <- log(df2$`Central Value ($ per ton CO2)`)
+df2$log.scc.2020usd[!is.finite(df2$log.scc.2020usd)] <- NA
+
+## Basic log model
+
+mod <- lm(log.scc.2020usd ~ `SCC Year` + `Year`, data=df2, weights=1 / df2$rows)
+
+## Predict into the future
+projdf <- tibble(Year=rep(2000:2100, 3), `SCC Year`=rep(c(2020, 2050, 2100), each=101))
+projdf <- cbind(projdf, predict(mod, projdf, interval='confidence'))
+projdf$scc.fit <- exp(projdf$fit + var(mod$resid)/2)
+projdf$scc.upr <- exp(projdf$upr + var(mod$resid)/2)
+projdf$scc.lwr <- exp(projdf$lwr + var(mod$resid)/2)
+
+ggplot(projdf, aes(Year, scc.fit, colour=factor(`SCC Year`))) +
+    geom_ribbon(data=subset(projdf, `SCC Year` == 2020), aes(ymin=scc.lwr, ymax=scc.upr), alpha=.5) +
+    geom_line(lwd=1) + scale_x_continuous(expand=c(0, 0)) +
+    scale_y_log10() + xlab("Year of publication") + ylab("SCC (2020 USD / tCO2)") +
+    theme_bw() + scale_colour_discrete(name="Pulse Year:")
+
+ggplot(projdf, aes(Year, scc.fit, colour=factor(`SCC Year`))) +
+    geom_line() + coord_cartesian(ylim=c(0, 10000)) +
+    xlab("Year of publication") + ylab("SCC (2020 USD / tCO2)") +
+    theme_bw()
+
+## Year of SCC surpassing VSL
+vsl <- 7.4e6 * gdp.deflator$factor[gdp.deflator$year == 2020] / gdp.deflator$factor[gdp.deflator$year == 2006]
+year.vsl <- log(vsl / exp(mod$coeff[1] + var(mod$resid)/2)) / sum(mod$coeff[2:3])
+
+## Try cubic polynomials
+
+df3 <- subset(df2, !is.na(`SCC Year`) & !is.na(`Year`))
+mod <- lm(`Central Value ($ per ton CO2)` ~ poly(`SCC Year`, 3) + poly(`Year`, 3), data=df3, weights=1 / df3$rows)
+
