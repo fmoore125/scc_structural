cc54ddfe74ff9fc528fab269a5fb323cbe949e30
fmoore125 <fmoore125@gmail.com>
2021-04-06 23:54:35 -0700

    first data check

--- src/analysis/full_distribution.R
+++ src/analysis/full_distribution.R
@@ -2,6 +2,7 @@ library(data.table)
 library(readxl)
 library(tidyverse)
 library(ggridges)
+library(viridisLite)
 
 source("src/analysis/find_distribution.R")
 source("src/data_cleaining_scripts/cleaning_master.R")
@@ -53,7 +54,9 @@ mod[c(grep("FUND",dat$`Base IAM (if applicable)`[dist$row]),grep("FUND",dat$`IAM
 mod[c(grep("PAGE",dat$`Base IAM (if applicable)`[dist$row]),grep("PAGE",dat$`IAM Calibrated To (if applicable)`[dist$row]))]="PAGE"
 mod[which(mod==0)]="Other"
 
-dist$mod=ordered(mod,levels=c("DICE","PAGE","FUND","Other"))
+dist$mod=ordered(mod,levels=c("Other","PAGE","FUND","DICE"))
 
-a=ggplot(dist[which(dist$draw>quantile(dist$draw,0.01)&dist$draw<quantile(dist$draw,0.99)),],aes(x=draw,y=mod,group=mod))+geom_density_ridges(scale=0.9)
-a=a+theme_bw()+labs(x="SCC ($ per ton CO2)",y="Base or Calibration Model")
+a=ggplot(dist[which(dist$draw>quantile(dist$draw,0.01)&dist$draw<quantile(dist$draw,0.99)),],aes(x=draw,y=mod,group=mod,fill=factor(stat(quantile))))
+a=a+stat_density_ridges(geom="density_ridges_gradient",calc_ecdf = TRUE,quantiles=4,quantile_lines = TRUE,bandwidth=2,scale=0.9)
+a=a+theme_bw()+labs(x="SCC ($ per ton CO2)",y="Base or Calibration Model")+ scale_fill_viridis_d(name = "Quartiles")
+a=a+theme(text=element_text(size=20))
---
+++ src/data_cleaining_scripts/datachecks.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(compare)
+
+dat=as.data.frame(read_excel("data/data_collection/SCC Meta-Analysis Data Template_Revised.xlsx",sheet="Data Entry"))
+colnames(dat)=dat[2,];dat=dat[-c(1:2),]
+names(dat)[duplicated(names(dat))] <- paste0(names(dat)[duplicated(names(dat))], '2')
+#check for rows that are identical except for SCC values
+
+#keep bibliographic, strucutral and parametric information and find unique values
+uniquevals=dat%>%
+  dplyr::select('ID_number','Base IAM (if applicable)','IAM Calibrated To (if applicable)','SCC Year','Scenario (e.g. Optimal, BAU)','Constant Discount Rate':'Alternative ethical approaches (not Discounted Utilitarianism)','TFP Growth':'PRTP2')
+
+uniquevals_dist=distinct(uniquevals)
+uniquevals_dist$rowid=1:nrow(uniquevals_dist)
+
+test=merge(uniquevals,uniquevals_dist,all=T,sort=FALSE)
+
+probs=names(which(table(test$rowid)>1))
+
+duplicatedrows=dat[which(test$rowid%in%probs),which(colnames(dat)%in%c("ID_number","Added By"))]
+duplicatedrows=distinct(duplicatedrows)
+write.csv(duplicatedrows,"outputs/duplicated.csv")
