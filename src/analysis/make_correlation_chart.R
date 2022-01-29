7b02f69a3d0253f14ce9344405ce43efc7425983
Ivan Rudik <irudik@cornell.edu>
2022-01-29 15:57:04 -0500

    add scc variability script

--- src/analysis/make_correlation_chart.R
+++ src/analysis/make_correlation_chart.R
@@ -10,27 +10,35 @@ options("tidylog.display" = NULL)
 # runs data cleaning to generate main dataframe as `dat`
 source("src/data_cleaining_scripts/cleaning_master.R")
 
+names_vec = c("PRTP", "EMUC", "Discount Rate", "Carbon Cycle", "Climate Model", "Climate Tipping", "Damage Tipping", "Persist/Growth Damage",
+              "Epstein-Zin", "Ambiguity", "Limited Substitutability", "Inequality Aversion", "Learning", "Alt. Ethical Approaches")
+
 # read in data for correlations
 # NAs are 0s so replace them
 M <- dat %>%
-  select(`Carbon Cycle`:`Alternative ethical approaches (not Discounted Utilitarianism)`) %>%
+  select(PRTP, EMUC, discountrate, `Carbon Cycle`:`Alternative ethical approaches (not Discounted Utilitarianism)`) %>%
   as_tibble() %>%
   mutate(across(everything(), ~ as.numeric(.x))) %>%
   replace(is.na(.), 0) %>%
   cor()
 
+rownames(M) = names_vec
+colnames(M) = names_vec
+
 # write the corr chart
-png("outputs/corr_chart.png", width = 10, height = 10, units = "in", res = 500)
+png("outputs/corr_chart.png", width = 15, height = 15, units = "in", res = 500)
 
 col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
 corrplot(M,
   method = "color", col = col(100),
-  type = "upper", order = "hclust",
+  type = "lower", order = "hclust",
   addCoef.col = "black", # Add coefficient of correlation
   tl.col = "black",
+  cl.cex = 1.5,
+  tl.cex = 1,
   # Combine with significance
   # hide correlation coefficient on the principal diagonal
-  diag = FALSE
+  diag = T
 )
 
 dev.off()
---
+++ src/analysis/test_scc_variability.R
@@ -0,0 +1,56 @@
+# Load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  data.table, janitor, magrittr, fixest,
+  broom, tidyverse, tidylog, mddelsummary
+)
+options("tidylog.display" = NULL)
+`%notin%` <- Negate(`%in%`)
+
+# load main dataset
+source("src/data_cleaining_scripts/cleaning_master.R")
+
+# select only the variables we need
+dat <- dat %>%
+  as_tibble() %>%
+  clean_names() %>%
+  rename(
+    damage_tipping = tipping_points2,
+    climate_tipping = tipping_points
+  ) %>%
+  select(
+    scc_year:discountrate
+  )
+
+# for each observation, construct the difference between
+# min and max values assuming a uniform distribution
+dat <- dat %>%
+  mutate(
+    unc_minmax = (max - min) / (1 - 0),
+    unc_999 = (x99_9th - x0_1th) / (.999 - .001),
+    unc_990 = (x99th - x1th) / (.99 - .01),
+    unc_975 = (x97_5th - x2_5th) / (.975 - .025),
+    unc_950 = (x95th - x5th) / (.95 - .05),
+    unc_900 = (x90th - x10th) / (.9 - .1),
+    unc_830 = (x83rd - x17th) / (.83 - .17),
+    unc_750 = (x75th - x25th) / (.75 - .25)
+  ) %>%
+  rowwise() %>%
+  # this is proportional to the standard deviation (if we only had one set of percentiles per obs)
+  # std dev of uniform = (max - min) / sqrt(12)
+  mutate(range = mean(c_across(unc_minmax:unc_750), na.rm = T))
+
+# variables on the RHS
+vars <- names(dat)[45:58]
+
+# ensure they are indicator variables
+dat <- dat %>%
+  mutate(across(tfp_growth:risk_aversion_ez_utility, ~ ifelse(is.na(.x), 0, 1)))
+
+a <- feols(
+  log(range) ~
+  .[vars]
+  + as.numeric(scc_year) + discountrate + central_value_per_ton_co2,
+  dat
+) %>%
+  summary()
