909c0e3f3887546d906bc1ed12acfea80843b5e4
fmoore125 <fmoore125@gmail.com>
2022-08-17 16:26:07 -0700

    adding survey results to figure 2

--- src/analysis/figures.R
+++ src/analysis/figures.R
@@ -7,6 +7,7 @@ library(MetBrewer)
 library(lfe)
 library(forcats)
 library(zoo)
+library(patchwork)
 
 dist=fread(file="outputs/distribution_v2.csv")
 dist_weighted=fread(file="outputs/distribution_coauthorweighted_v2.csv")
@@ -56,8 +57,7 @@ dist$yeargroup=fct_recode(dist$yeargroup,'<2010'="(1.99e+03,2.01e+03]",'2010-203
 
 distplot=dist
 distplot$y=ifelse(distplot$yeargroup%in%c('2010-2030','2030-2070'),-0.004,-0.006)
-distplot=distplot%>%filter(yeargroup%in%c('2010-2030','2030-2070',"2070-2100"))%>%
-  filter(draw>quantile(draw,0.005)&draw<quantile(draw,0.995))
+distplot=distplot%>%filter(yeargroup%in%c('2010-2030','2030-2070',"2070-2100"))
 
 quants=c(0.01,0.05,0.25,0.5,0.75,0.95,0.99)
 pfuns=map(quants,~partial(quantile,probs=.x,na.rm=T))
@@ -114,10 +114,10 @@ surveydat_summary$y=c(-0.015,-0.02)
 surveydat_summary$highest[which(surveydat_summary$highest>1500)]=1499
 
 a=a+geom_boxplot(data=surveydat_summary%>%filter(type=="Lit"),aes(x=y,min=min,lower=lower,middle=middle,upper=upper,max=max,group=yeargroup),inherit.aes=FALSE,width=0.0035,fill="white",col=cols[1],stat="identity")
-a=a+geom_segment(data=surveydat_summary%>%filter(type=="Lit"),aes(x=y,xend=y,y=lowest,yend=min),lty=2)+geom_segment(data=surveydat_summary%>%filter(type=="Lit"),aes(x=y,xend=y,y=highest,yend=max),lty=2,col=cols[1])
+a=a+geom_segment(data=surveydat_summary%>%filter(type=="Lit"),aes(x=y,xend=y,y=lowest,yend=min),lty=2,col=cols[1])+geom_segment(data=surveydat_summary%>%filter(type=="Lit"),aes(x=y,xend=y,y=highest,yend=max),lty=2,col=cols[1])
 
 a=a+geom_boxplot(data=surveydat_summary%>%filter(type=="Tru"),aes(x=y,min=min,lower=lower,middle=middle,upper=upper,max=max,group=yeargroup),inherit.aes=FALSE,width=0.0035,fill="white",col=cols[2],stat="identity")
-a=a+geom_segment(data=surveydat_summary%>%filter(type=="Tru"),aes(x=y,xend=y,y=lowest,yend=min),lty=2)+geom_segment(data=surveydat_summary%>%filter(type=="Tru"),aes(x=y,xend=y,y=highest,yend=max),lty=2,col=cols[2])
+a=a+geom_segment(data=surveydat_summary%>%filter(type=="Tru"),aes(x=y,xend=y,y=lowest,yend=min),lty=2,col=cols[2])+geom_segment(data=surveydat_summary%>%filter(type=="Tru"),aes(x=y,xend=y,y=highest,yend=max),lty=2,col=cols[2])
 
 ann_text2=data.frame(text=c("Expert Survey: Literature 2020 Estimate","Expert Survey: Comprehensive 2020 Estimate"),yeargroup=factor(rep("2010-2030",2)),y=c(1000,1000),x=c(-0.013,-0.018),type=c("Lit","Tru"))
 a=a+geom_text(data=ann_text2,aes(y=y,x=x,label=text,col=type),inherit.aes=FALSE)
@@ -185,7 +185,7 @@ struc=dat%>%
 
 diststruc=cbind(dist,struc[dist$row,])
 
-diststruc=diststruc%>%rename("Tipping Points: Climate"="Tipping Points","Tipping Points: Damages"="Tipping Points2","Limited Substitutability"="Limitedly-Substitutable Goods")
+diststruc=diststruc%>%dplyr::rename("Tipping Points: Climate"="Tipping Points","Tipping Points: Damages"="Tipping Points2","Limited Substitutability"="Limitedly-Substitutable Goods")
 
 diststruc=diststruc%>%
   pivot_longer(cols="Earth System":"Learning",names_to="StructuralChange",values_to="Presence")
@@ -193,19 +193,20 @@ diststruc=diststruc%>%
 diststruc$Presence=fct_collapse(diststruc$Presence,No=c("-1.0","0"),Yes=c("1.0","Calibrated",1))
 
 diststrucdensities=diststruc%>%
+  filter(yeargroup=="2010-2030")%>%
   group_by(StructuralChange)%>%
-  filter(Presence==1&draw>0)%>%
+  filter(Presence=="Yes"&draw>0)%>%
   mutate(logscc=log(draw))%>%
   group_modify(~ ggplot2:::compute_density(.x$logscc, NULL,bw=0.4))%>%
-  rename(logscc=x)
+  dplyr::rename(logscc=x)
 
 #find rows with no structural changes at all for "reference" density
 reference=which(apply(struc,MARGIN=1,FUN=function(x) sum(x=="0"))==9)
 referencedensity=diststruc%>%
-  filter(row%in%reference&draw>0)%>%
+  filter(yeargroup=="2010-2030"&row%in%reference&draw>0)%>%
   mutate(logscc=log(draw))%>%
   group_modify(~ ggplot2:::compute_density(.x$logscc, NULL,bw=0.4))%>%
-  rename(logscc=x)
+  dplyr::rename(logscc=x)
 
 changes=unique(diststruc$StructuralChange);referencedensity$StructuralChange=changes[1]
 for(i in 2:length(changes)){temp=referencedensity[,1:6];temp$StructuralChange=changes[i];referencedensity=rbind(referencedensity,temp)}
@@ -220,17 +221,43 @@ diststrucdensities$StructuralChange=ordered(diststrucdensities$StructuralChange,
 diststruc$paper=dat$ID_number[diststruc$row]
 papers=diststruc%>%
   group_by(StructuralChange)%>%
-  filter(Presence==1)%>%
+  filter(Presence=="Yes"&yeargroup=="2010-2030")%>%
   dplyr::summarise(npapers=length(unique(paper)),n=length(unique(row)))
 
 diststrucdensities$type=as.factor(diststrucdensities$type)
 
 breaks_ln=c(0,2.5,5,7.5,10)
 a=ggplot(diststrucdensities,aes(x=logscc,y=StructuralChange,height=density,group=interaction(type,StructuralChange),fill=as.factor(type)))+geom_density_ridges(stat = "identity",scale=0.92,lwd=1)
-a=a+theme_ridges()+theme_bw()+theme(text=element_text(size=18))+labs(x="SCC (2020 $ per ton CO2)",y="",fill="")
-a=a+scale_x_continuous(limits=c(0,10),breaks=breaks_ln,labels=c(round_any(exp(breaks_ln[1:3]),10),round_any(exp(breaks_ln[4:5]),100)))+scale_fill_manual(values=c("steelblue4",NA))
+a=a+theme_ridges()+theme_bw()+theme(text=element_text(size=18),legend.position="top")+labs(x="2020 SCC (2020 $ per ton CO2)",y="",fill="")
+a=a+scale_x_continuous(limits=c(0,10),breaks=breaks_ln,labels=c(round_any(exp(breaks_ln[1:3]),10),round_any(exp(breaks_ln[4:5]),100)))+scale_fill_manual(values=c("steelblue4",NA),na.value=NA)
 a=a+geom_text(data=papers,aes(label=paste0("n=",npapers," (",n,")"),y=StructuralChange,x=9.2),inherit.aes=FALSE,size=6,nudge_y=0.35)
+
+#add in figure 2 data
+load(file="outputs/expert_survey_data_products/fig2surveydata.Rdat")
+
+#use reference value given in survey of $41.50 to convert % changes into SCC values
+refval=41.5
+helper=function(x) log((1+x/100)*refval)
+for(i in 2:10) fig2dat_vals[,i]=helper(fig2dat_vals[,i])
+fig2dat_vals=pivot_longer(fig2dat_vals,cols=2:10,names_to="StructuralChange",values_to="logscc")
+
+a=a+geom_density_ridges2(data=fig2dat_vals,aes(x=logscc,y=StructuralChange,group=StructuralChange),fill="darkgoldenrod",inherit.aes=FALSE,stat = "binline", binwidth = 0.1,scale=0.88)
+a=a+geom_vline(xintercept=log(refval),lty=3,col="#36c687",lwd=1)
 a
+
+#add graph to the side showing histograms of assessed quality
+fig2dat_qual=fig2dat_qual%>%
+  pivot_longer(2:10,names_to="StructuralChange",values_to="ans")%>%
+  filter(!is.na(ans))
+fig2dat_qual$ans=as.factor(fig2dat_qual$ans)
+fig2dat_qual$ans=fct_relevel(fig2dat_qual$ans,"Strongly Disagree","Disagree","Neither Agree nor Disagree","Agree","Strongly Agree")
+
+b=ggplot(fig2dat_qual,aes(x=ans,y=StructuralChange,group=StructuralChange))
+b=b+geom_density_ridges2(fill="#f7e057",col="black",stat = "binline", binwidth = 1,scale=0.95)
+b=b+theme_bw()+theme(text=element_text(size=12),axis.text.y=element_blank(),axis.text.x = element_text(angle = 45))+labs(y="",x="")
+
+a+b+plot_layout(widths=c(3,1))
+
 
 #calculate means
 means=diststrucdensities%>%
