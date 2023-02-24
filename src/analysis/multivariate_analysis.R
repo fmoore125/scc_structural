1f3cc88c831ef7a740d60ee63a5ef833e3957653
James Rising <jarising@gmail.com>
2023-02-24 14:01:13 -0500

    add anova

--- src/analysis/multivariate_analysis.R
+++ src/analysis/multivariate_analysis.R
@@ -80,6 +80,96 @@ mod_paperfe=feols(fml=I(log(draw))~Earth_system_struc+Tipping.Points_struc+Tippi
 #             Equilibrium.Climate.Sensitivity_param+Tipping.Point.Magnitude_param+Damage.Function_param+Adaptation.Rates_param+Income.Elasticity_param+
 #             Constant.Discount.Rate_param+EMUC2_param+PRTP2_param+Risk.Aversion..EZ.Utility._param+sccyear_from2020+I(sccyear_from2020^2)+discountrate+I(discountrate^2)+declining+dicemodel+fundmodel+pagemodel+backstop+failure+log.scc.synth + missing.scc.synth|paper | 0 | row,data=distreg[which(distreg$draw>0),])
 
+distreg$whichmodel <- 'other'
+distreg$whichmodel[distreg$dicemodel == 1] <- 'dice'
+distreg$whichmodel[distreg$fundmodel == 1] <- 'fund'
+distreg$whichmodel[distreg$pagemodel == 1] <- 'page'
+
+labels <- data.frame(pred=c('Transient.Climate.Response_param', 'Carbon.Cycle2_param', 'Equilibrium.Climate.Sensitivity_param',
+                            'Earth_system_struc',
+                            'Tipping.Points_struc', 'Tipping.Point.Magnitude_param', 'Tipping.Points2_struc',
+                            'Persistent...Growth.Damages_struc', 'Limitedly.Substitutable.Goods_struc', 'Inequality.Aversion_struc',
+                            'Risk.Aversion..EZ.Utility._param', 'Epstein.Zin_struc',
+                            'whichmodel', 'Ambiguity.Model.Uncertainty_struc',
+                            'Learning_struc', 'failure',
+                            'TFP.Growth_param', 'Population.Growth_param', 'Emissions.Growth_param',
+                            'Constant.Discount.Rate_param', 'discountrate', 'I(discountrate^2)',
+                            'PRTP2_param', 'EMUC2_param',
+                            'log.scc.synth', 'missing.scc.synth',
+                            'Damage.Function_param', 'Adaptation.Rates_param', 'Income.Elasticity_param'),
+                     type=c(rep('param', 3),
+                             'struc',
+                             'struc', 'param', 'struc',
+                             'struc', 'struc', 'struc',
+                             'param', 'struc',
+                             'struc', 'param', # NOTE: treating model unc. as parametric
+                             'struc', 'struc',
+                             rep('param', 3),
+                             'param', 'value', 'value',
+                             'param', 'param',
+                             'value', 'value',
+                             rep('param', 3)),
+                     label=c(rep('Earth System', 3),
+                             'Earth System',
+                             'Climate Tipping Points', 'Damages Tipping Points', 'Damages Tipping Points',
+                             'Growth Damages', 'Limited Substitutability', 'Distributional Weights',
+                             'Epstein-Zin', 'Epstein-Zin',
+                             'Model', 'Model', # Label as model uncertainty
+                             'Learning', 'Market Failures',
+                             rep('Socioeconomic Uncertainty', 3),
+                             rep('Discounting', 3),
+                             'Pure Time Preference', 'Elasticity of Marginal Utility',
+                             'Damage Function Parameters', 'Damage Function Parameters',
+                             rep('Damage Function Parameters', 3)),
+                     color=c(rep('#a6611a', 3),
+                             '#a6611a',
+                             '#dfc27d', '#fed976', '#fed976',
+                             '#feb24c', '#a1d99b', '#74c476',
+                             '#31a354', '#31a354',
+                             rep('#006d2c', 2),
+                             '#9ecae1', '#3182bd',
+                             rep('#08519c', 3),
+                             rep('#de8682', 3),
+                             '#de2d26', '#a50f15',
+                             '#54278f', '#54278f',
+                             rep('#54278f', 3)))
+
+mod_paperfe2 <- lm(log(draw)~Earth_system_struc+Tipping.Points_struc+Tipping.Points2_struc+Persistent...Growth.Damages_struc+
+            Epstein.Zin_struc+Ambiguity.Model.Uncertainty_struc+Limitedly.Substitutable.Goods_struc+Inequality.Aversion_struc+
+            Learning_struc+TFP.Growth_param+Population.Growth_param+Emissions.Growth_param+Transient.Climate.Response_param+Carbon.Cycle2_param+
+            Equilibrium.Climate.Sensitivity_param+Tipping.Point.Magnitude_param+Damage.Function_param+Adaptation.Rates_param+Income.Elasticity_param+
+            Constant.Discount.Rate_param+EMUC2_param+PRTP2_param+Risk.Aversion..EZ.Utility._param+discountrate+I(discountrate^2)+declining+whichmodel+backstop+failure+log.scc.synth + missing.scc.synth + factor(paper),data=distreg[which(distreg$draw>0 & distreg$sccyear_from2020 >= -10 & distreg$sccyear_from2020 <= 10),])
+
+anv <- anova(mod_paperfe2)
+anvdf <- as.data.frame(anv)
+anvdf$pred <- rownames(anvdf)
+
+anvdf <- anvdf[!(anvdf$pred %in% c("Residuals", "factor(paper)")),]
+
+stopifnot(nrow(anvdf %>% left_join(labels)) == nrow(anvdf))
+anvdf2 <- labels %>% left_join(anvdf)
+anvdf2$pred <- factor(anvdf2$pred, levels=anvdf2$pred)
+anvdf2$cumss <- 1 - cumsum(anvdf2$`Sum Sq`) / sum(anvdf2$`Sum Sq`)
+anvdf2.label <- anvdf2 %>% group_by(label) %>% summarize(cumss.lo=min(cumss)) %>% arrange(cumss.lo)
+anvdf2.label$cumss.hi <- c(anvdf2.label$cumss.lo[-1], 1)
+
+anvdf2.label$`Sum Sq` <- (anvdf2.label$cumss.hi + anvdf2.label$cumss.lo) / 2
+anvdf2.label$width <- anvdf2.label$cumss.hi - anvdf2.label$cumss.lo
+
+ggplot(anvdf2, aes(x=1, y=`Sum Sq`)) +
+    geom_bar(aes(group=pred, fill=color, linetype=type), colour='#000000',
+             linewidth=.25, stat="identity", position="fill", width=1) +
+    geom_text(data=subset(anvdf2.label, width > .002), aes(label=label, hjust='left'), nudge_x=.65, size=2) +
+    geom_label(data=subset(anvdf2.label, width > .002 & label != "Epstein-Zin"), aes(label=round(width * 100, 0)), size=2) +
+    geom_label(data=subset(anvdf2.label, label == "Epstein-Zin"), aes(label=round(width * 100, 0)), nudge_y=-.02, size=2) +
+    scale_fill_identity() + scale_linetype_manual("Source of\nVariation\n(outline):",
+                                                  breaks=c('struc', 'param', 'value'), values=c('solid', 'blank', 'dashed'),
+                                                  labels=c("Structural", "Parametric", "Specified")) +
+    scale_y_continuous("Percent of explained variance", labels=scales::percent) + xlim(0.5, 6) + xlab(NULL) +
+    theme_bw() + theme(legend.justification=c(1,1), legend.position=c(.995,.995),
+                            axis.text.x=element_blank(), axis.ticks.x=element_blank())
+ggsave("figures/anova.pdf", width=3.5, height=5)
+
 varnames=c("sccyear_from2020" ="SCC Year","I(I(sccyear_from2020^2))" ="SCC Year^2","discountrate" ="Discount Rate","I(I(discountrate^2))"="Discount Rate^2","declining"="Declining DR","Earth_system_strucYes"="Earth System", "Tipping.Points_strucYes" ="Climate Tipping Points","Tipping.Points2_strucYes"="Damages Tipping Points", "Persistent...Growth.Damages_strucYes"="Growth Damages","Epstein.Zin_strucYes"="Epstein Zin","Ambiguity.Model.Uncertainty_strucYes"="Ambiguity","Limitedly.Substitutable.Goods_strucYes" ="Limited-Substitutability","Inequality.Aversion_strucYes"="Inequality Aversion","Learning_strucYes"="Learning","TFP.Growth_paramYes"="TFP Growth","Population.Growth_paramYes"="Pop Growth","Emissions.Growth_paramYes"="Emissions Growth", "Transient.Climate.Response_paramYes" ="Trans. Climate Resp.","Carbon.Cycle2_paramYes" ="Carbon Cycle (Param)","Equilibrium.Climate.Sensitivity_paramYes"="Eqm. Climate Sens.", "Tipping.Point.Magnitude_paramYes"="Tipping Point Size","Damage.Function_paramYes"="Damage Function","Adaptation.Rates_paramYes" ="Adaptation Rates","Income.Elasticity_paramYes"="Income Elasticity","Constant.Discount.Rate_paramYes" ="Const. Discount Rate","EMUC2_paramYes" ="EMUC", "PRTP2_paramYes"="PRTP","Risk.Aversion..EZ.Utility._paramYes" ="Risk Aversion", "dicemodel"="DICE","fundmodel"="FUND","pagemodel" ="PAGE","backstop"="Backstop Price","failure"="Other Market Failure","log.scc.synth"="Damage-based SCC")
 
 modelsummary(models=list(mod_ols,mod_paperfe),coef_rename = varnames,output="outputs\\sccolsanalysis.docx",stars=TRUE)
