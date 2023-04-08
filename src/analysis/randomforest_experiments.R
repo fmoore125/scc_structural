edc52bd49672fc56cf5a89b916c16961afa45ce9
fmoore125 <fmoore125@gmail.com>
2023-04-08 10:43:29 -0700

    add final random forest experiments

--- src/analysis/randomforest_experiments.R
+++ src/analysis/randomforest_experiments.R
@@ -31,7 +31,7 @@ rel=which(dat$`Damage Function Info: Model, Commonly-Used Function, or Function`
 sampdat_DICE$log.scc.synth=sample(dat$log.scc.synth[rel],samppred,replace=TRUE)
 sampdat_DICE$declining="No"
 predictions_DICE=predict(rfmod,sampdat_DICE)
-predictions_DICE=predictions_DICE$predictions+sample(rfmod_explained$residuals,samppred,replace=TRUE)
+#predictions_DICE=predictions_DICE$predictions+sample(rfmod_explained$residuals,samppred,replace=TRUE)
 fwrite(as.data.table(predictions_DICE),file="outputs/rf_experiments/A_DICE.csv")
 
 #B. EPA assumptions
@@ -52,7 +52,7 @@ rel=which(dat$`Damage Function Info: Model, Commonly-Used Function, or Function`
 sampdat_EPA$log.scc.synth=sample(dat$log.scc.synth[rel],samppred,replace=TRUE)
 
 predictions_EPA=predict(rfmod,sampdat_EPA)
-predictions_EPA=predictions_EPA$predictions+sample(rfmod_explained$residuals,samppred,replace=TRUE)
+#predictions_EPA=predictions_EPA$predictions+sample(rfmod_explained$residuals,samppred,replace=TRUE)
 
 fwrite(as.data.table(predictions_EPA),file="outputs/rf_experiments/B_EPA.csv")
 
@@ -61,13 +61,51 @@ fwrite(as.data.table(predictions_EPA),file="outputs/rf_experiments/B_EPA.csv")
 sampdat_allstruc=sampdat[sample(1:nrow(sampdat),samppred,replace=FALSE),]
 sampdat_allstruc[,grep("_struc",colnames(sampdat_allstruc))]="Yes"
 predictions_allstruc=predict(rfmod,sampdat_allstruc)
-predictions_allstruc=predictions_allstruc$predictions+sample(rfmod_explained$residuals,samppred,replace=TRUE)
+#predictions_allstruc=predictions_allstruc$predictions+sample(rfmod_explained$residuals,samppred,replace=TRUE)
 
 fwrite(as.data.table(predictions_allstruc),file="outputs/rf_experiments/C_allstruc.csv")
 
-#E. Multiple discount rates (2, 3, 5, declining)
+#E. Multiple constant discount rates (1, 1.5, 2.5, 3, 5)
+discs=c(1,1.5,2.5,3,5)
+
+for(i in 1:length(discs)){
+  sampdat_discs=sampdat[sample(1:nrow(sampdat),samppred,replace=FALSE),]
+  sampdat_discs$discountrate=discs[i];sampdat_discs$declining="No"
+  predictions_disc=predict(rfmod,sampdat_discs)
+  fwrite(as.data.table(predictions_disc),file=paste0("outputs/rf_experiments/E_discountrates_",discs[i],".csv"))
+  print(i)
+}
+
 #F. Multiple publication years (2000, 2010, 2020)
+pubyears=c(2000,2010,2020)
+
+for(i in 1:length(pubyears)){
+  sampdat_pubs=sampdat[sample(1:nrow(sampdat),samppred,replace=FALSE),]
+  sampdat_pubs$PublicationYear=pubyears[i]
+  predictions_pubs=predict(rfmod,sampdat_pubs)
+  fwrite(as.data.table(predictions_pubs),file=paste0("outputs/rf_experiments/F_pubyears_",pubyears[i],".csv"))
+  print(i)
+}
+
 #G. Multiple SCC pulse years (2020, 2050) - done in randomforest.R
 #H. Multiple synthetic SCCs (DICE, FUND, PAGE, Howard & Sterner)
+
+damages=c(11.72858,6.337801,14.02219,38.60881)
+damagemods=c("DICE2016r2","FUND38","PAGE2009","HowardSterner")
+
+#From James: DICE 2016r2: 11.72858
+#FUND 3.8: 6.337801
+#PAGE 2009: 14.02219
+#Howard & Sterner: 38.60881
+
+for(i in 1:length(damages)){
+  sampdat_damages=sampdat[sample(1:nrow(sampdat),samppred,replace=FALSE),]
+  sampdat_damages$log.scc.synth=log(damages[i])
+  predictions_damages=predict(rfmod,sampdat_damages)
+  fwrite(as.data.table(predictions_damages),file=paste0("outputs/rf_experiments/H_damages_",damagemods[i],".csv"))
+  print(i)
+}
+
+
 
 
